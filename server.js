0133507c3cef177920e9895e21aa83b406443350
Basavaraju VB <167985344+BasavarajuVB@users.noreply.github.com>
2024-11-23 17:19:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require("./config/db")
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// // Database connection
+// mongoose.connect(process.env.MONGODB_URI)
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+connectDB();
+
+// Routes
+app.use('/api/notes', require('./routes/notes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
